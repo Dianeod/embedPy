a6dc3ad50ddac66583b86be35320d100cad29b8c
Dianeod <dodonoghue@kx.com>
2020-08-03 13:38:57 +0100

    removed python3 embed

--- build/build.bat
+++ build/build.bat
@@ -15,8 +15,7 @@ set PATH=C:\Perl;%PATH%
 perl -p -i.bak -e s/EMBEDPYVERSION/`\$\"%EMBEDPY_VERSION%\"/g p.q
 
 mkdir w64
-python3-config --libs --embed
-cl /LD /lpython3.8 /DKXVER=3 /Fep.dll /O2 py.c q.lib                                  || goto :error
+cl /LD  /DKXVER=3 /Fep.dll /O2 py.c q.lib                                  || goto :error
 move p.dll w64
 set PATH=%OP%
 :: Conda build
